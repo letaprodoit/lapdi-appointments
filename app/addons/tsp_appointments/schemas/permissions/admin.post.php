8ef3508ddb89917d682eccfaccb7827d576b5c85
Sharron Denice <sharron@thesoftwarepeople.com>
2014-02-28 18:07:46 -0600

    - Fixed issue with permissions

--- app/addons/tsp_appointments/schemas/permissions/admin.post.php
+++ app/addons/tsp_appointments/schemas/permissions/admin.post.php
@@ -4,15 +4,13 @@
  *
  * @package		TSP Appointments for CS-Cart
  * @filename	admin.post.php
- * @version		2.0.5
+ * @version		2.0.6
  * @author		Sharron Denice, The Software People, LLC on 2013/02/09
  * @copyright	Copyright © 2013 The Software People, LLC (www.thesoftwarepeople.com). All rights reserved
  * @license		Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported (http://creativecommons.org/licenses/by-nc-nd/3.0/)
  * @brief		Admin post permissions for menus
  * 
  */
-
-$schema = array();
 
 $schema['appointments'] = array (
 	'permissions' => 'manage_appointments',
